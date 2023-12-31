e15ea67bc3797ba39d39ce4e4044b23f90ba8212
HAMMADMIRFAN <DEV.HAMMAD1@GMAIL.COM>
2023-12-31 10:01:29 -0800

    commit

---
+++ if_else.js
@@ -0,0 +1,12 @@
+// <Conditional Statements
+// Very often when you write code, you want to perform different actions for different decisions.
+
+// You can use conditional statements in your code to do this.
+
+// In JavaScript we have the following conditional statements:
+
+// Use if to specify a block of code to be executed, if a specified condition is true
+// Use else to specify a block of code to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+// Use switch to specify many alternative blocks of code to be executed>
+
---
+++ operators.js
@@ -0,0 +1,71 @@
+
+
+// What is an Operator?
+// In JavaScript, an operator is a special symbol used to perform operations on operands (values and variables). For example,
+
+2 + 3; // 5
+// Here + is an operator that performs addition, and 2 and 3 are operands.
+
+
+// avaScript Operator Types
+// Here is a list of different operators you will learn in this tutorial.
+
+// Assignment Operators
+// Arithmetic Operators
+// Comparison Operators
+// Logical Operators
+// Bitwise Operators
+// String Operators
+// Other Operators
+
+// arithmetic operators
+
+let x = 5;
+let y = 3;
+
+// addition
+console.log('x + y = ', x + y);  // 8
+
+// subtraction
+console.log('x - y = ', x - y);  // 2
+
+// multiplication
+console.log('x * y = ', x * y);  // 15
+
+// division
+console.log('x / y = ', x / y);  // 1.6666666666666667
+
+// remainder
+console.log('x % y = ', x % y);   // 2
+
+// increment
+console.log('++x = ', ++x); // x is now 6
+console.log('x++ = ', x++); // prints 6 and then increased to 7
+console.log('x = ', x);     // 7
+
+// decrement
+console.log('--x = ', --x); // x is now 6
+console.log('x-- = ', x--); // prints 6 and then decreased to 5
+console.log('x = ', x);     // 5
+
+//exponentiation
+console.log('x ** y =', x ** y);
+
+// comparision operators
+// equal operator
+console.log(2 == 2); // true
+console.log(2 == '2'); // true
+
+// not equal operator
+console.log(3 != 2); // true
+console.log('hello' != 'Hello'); // true
+
+// strict equal operator
+console.log(2 === 2); // true
+console.log(2 === '2'); // false
+
+// strict not equal operator
+console.log(2 !== '2'); // true
+console.log(2 !== 2); // false
+
+
